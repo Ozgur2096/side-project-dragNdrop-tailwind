9af32ead904c90f761030e9eded63674381cf3f6
Ozgur Ozhan <ozgur2096@gmail.com>
2023-09-07 09:10:26 +0200

    ListTasks.jsx modified

--- src/components/ListTasks.jsx
+++ src/components/ListTasks.jsx
@@ -55,19 +55,47 @@ const Section = ({ state, tasks, setTasks, todos, inProgress, closed }) => {
 
   return (
     <div className='w-64'>
-      <Header text={text} bg={bg} count={tasksToMap.length} /> List
+      <Header text={text} bg={bg} count={tasksToMap.length} />
+      {tasksToMap.length > 0 &&
+        tasksToMap.map(task => (
+          <Task key={task.id} task={task} tasks={tasks} setTasks={setTasks} />
+        ))}
     </div>
   );
 };
 const Header = ({ text, bg, count }) => {
   return (
     <div
-      className={`${bg} flex items-center h-12 pt-4 rounded-md uppercase text-sm text-white`}
+      className={`${bg} flex items-center h-12 pt-4 rounded-md uppercase text-sm text-white p-4`}
     >
       {text}{' '}
       <div className='ml-2 bg-white w-5 h-5 text-black rounded-full flex items-center justify-center'>
         {count}
       </div>
+    </div>
+  );
+};
+
+const Task = ({ task, tasks, setTasks }) => {
+  return (
+    <div className='relative p-4 mt-8 shadow-md rounded-md cursor-grab'>
+      <p>{task.name}</p>
+      <button className='absolute bottom-1 right-1 text-slate-400'>
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          fill='none'
+          viewBox='0 0 24 24'
+          strokeWidth='1.5'
+          stroke='currentColor'
+          className='w-6 h-6'
+        >
+          <path
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            d='M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z'
+          />
+        </svg>
+      </button>
     </div>
   );
 };
